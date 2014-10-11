e396d93d2549a157bff4a26589b3e5d7c980f330
smlevorse <smlevorse@msn.com>
2014-10-11 17:44:08 -0400

    Added register.php

---
+++ register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Register - The Writer's Grind</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <?php
+            if (isset($_POST['submit'])) {
+                echo "FORM SUBMITTED SUCCESSFULLY";
+            }
+        ?>
+        
+        <!-- Add your site or application content here -->
+        <img id="logo" src="res/logo.png" alt="The Writer's Grind logo"/>
+        <form name="login" action="index.php" method="POST">
+            <label for="username">Username: </label>
+            <input type="text" name="username">
+            <label for="password">Password: </label>
+            <input type="text" name="password">
+            <input type="Submit" name="submit" value="Register"/>
+        </form>
+
+        <form name="register" action="register.php" method="POST">
+            <label for="username">Username: </label>
+            <input type="text" name="username">
+            <label for="email">E-Mail: </label>
+            <input type="text" name="email">
+            <label for="password">Password: </label>
+            <input type="text" name="password">
+            <label for="confirm">Confirm: </label>
+            <input type="text" name="confirm">
+            <label for="bio">Biography: </label>
+            <input type="text" name="bio">
+        </form>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
